f008a75261c02f62eea5faab4a726765f69831eb
Kiran Shila <me@kiranshila.com>
2023-01-22 14:42:32 -0800

    test file

--- set_cap_perms.sh
+++ set_cap_perms.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
-sudo setcap cap_net_raw,cap_net_admin+ep target/release/grex_t0
+BINARY=grex_t0
+if test -f "target/debug/$BINARY"; then
+    sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
+fi
+if test -f "target/release/$BINARY"; then
+    sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
+fi

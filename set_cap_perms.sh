c6b555ec3b42bbf4eb611b2d5dfacba77b17c335
Kiran Shila <me@kiranshila.com>
2023-01-22 14:40:30 -0800

    update binary name

--- set_cap_perms.sh
+++ set_cap_perms.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex-t0
+sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
+sudo setcap cap_net_raw,cap_net_admin+ep target/release/grex_t0

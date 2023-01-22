3f2c111ecfc88e1393dff54f3fd663fece144233
Kiran Shila <me@kiranshila.com>
2023-01-22 15:54:56 -0800

    Don't log on backed up errors

--- src/capture.rs
+++ src/capture.rs
@@ -2,9 +2,9 @@
 
 use crate::common::Payload;
 use crossbeam_channel::{Receiver, Sender};
+use log::{info, warn};
 use num_complex::Complex;
 use pcap::Stat;
-use log::{info, warn};
 
 /// FPGA UDP "Word" size (8 bytes as per CASPER docs)
 const WORD_SIZE: usize = 8;
@@ -107,15 +107,11 @@ pub fn pcap_task(
                 .is_ok()
             {
                 // We don't care about dropping stats, we *do* care about dropping packets
-            } else {
-                warn!("Stats channel backed up");
             }
         }
         if let Some(payload) = cap.next_payload() {
             if packet_sender.try_send(payload).is_ok() {
                 count += 1;
-            } else {
-                warn!("Packet channel backed up");
             }
         }
     }
@@ -129,8 +125,6 @@ pub fn decode_task(packet_receiver: &Receiver<RawPacket>, payload_sender: &Sende
         };
         if payload_sender.try_send(Payload::from_bytes(&v)).is_ok() {
             // If this channel backs up, we don't care, drop packets upstream
-        } else {
-            warn!("Payload channel backed up");
         }
     }
 }

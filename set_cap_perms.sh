bd38a87287892a629d7db13025f16ff4a9d5751f
Kiran Shila <me@kiranshila.com>
2023-01-22 14:42:59 -0800

    typo

--- set_cap_perms.sh
+++ set_cap_perms.sh
@@ -4,5 +4,5 @@ if test -f "target/debug/$BINARY"; then
     sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
 fi
 if test -f "target/release/$BINARY"; then
-    sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
+    sudo setcap cap_net_raw,cap_net_admin+ep target/release/grex_t0
 fi

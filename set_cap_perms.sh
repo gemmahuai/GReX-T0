40a8c85e15a567d56e8c8028eca1727dcec90ac0
Kiran Shila <me@kiranshila.com>
2023-02-03 13:38:28 -0800

    update dependencies

--- set_cap_perms.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-BINARY=grex_t0
-if test -f "target/debug/$BINARY"; then
-    sudo setcap cap_net_raw,cap_net_admin+ep target/debug/grex_t0
-fi
-if test -f "target/release/$BINARY"; then
-    sudo setcap cap_net_raw,cap_net_admin+ep target/release/grex_t0
-fi

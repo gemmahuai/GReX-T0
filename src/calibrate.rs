562cb1cc8ae0bba5ecfbdd51271bb0517d8b8611
Kiran Shila <kshila@caltech.edu>
2023-08-15 14:24:10 -0700

    More lambda, less order

--- src/calibrate.rs
+++ src/calibrate.rs
@@ -6,8 +6,8 @@ use std::{fs, time::Duration};
 use whittaker_smoother::whittaker_smoother;
 
 const CALIBRATION_ACCUMULATIONS: u32 = 131072; // Around 1 second at 8.192us
-const SMOOTH_LAMBDA: f64 = 10.0;
-const SMOOTH_ORDER: usize = 5;
+const SMOOTH_LAMBDA: f64 = 15.0;
+const SMOOTH_ORDER: usize = 3;
 
 fn write_to_file(data: &[f64], filename: &str) {
     fs::write(

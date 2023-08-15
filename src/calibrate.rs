6ef712bb7fb106a419ae88e9d771332bdcb144a1
Kiran Shila <kshila@caltech.edu>
2023-08-15 12:38:38 -0700

    polyfit

--- src/calibrate.rs
+++ src/calibrate.rs
@@ -1,9 +1,12 @@
 //! Pre pipeline calibration routine
 
 use crate::{common::PACKET_CADENCE, fpga::Device};
+use eyre::eyre;
+use nalgebra::{DMatrix, DVector, RealField, SVD};
 use std::{fs, time::Duration};
 
 const CALIBRATION_ACCUMULATIONS: u32 = 131072; // Around 1 second at 8.192us
+const FIT_ORDER: usize = 6;
 
 fn write_to_file(data: &[f64], filename: &str) {
     fs::write(
@@ -14,6 +17,32 @@ fn write_to_file(data: &[f64], filename: &str) {
             .join("\n"),
     )
     .unwrap();
+}
+
+pub fn polyfit<T: RealField + Copy>(
+    x_values: &[T],
+    y_values: &[T],
+    polynomial_degree: usize,
+) -> eyre::Result<Vec<T>> {
+    // Number of terms (x^0 + x^2 ...)
+    let cols = polynomial_degree + 1;
+    let rows = x_values.len();
+    // Setup the linear regression problem
+    let mut a = DMatrix::zeros(rows, cols);
+    for (row, &x) in x_values.iter().enumerate() {
+        // First column is always 1
+        a[(row, 0)] = T::one();
+        for col in 1..cols {
+            a[(row, col)] = x.powf(nalgebra::convert(col as f64));
+        }
+    }
+    // Setup the b side
+    let b = DVector::from_row_slice(y_values);
+    let decomp = SVD::new(a, true, true);
+    match decomp.solve(&b, nalgebra::convert(1e-18f64)) {
+        Ok(mat) => Ok(mat.data.into()),
+        Err(error) => Err(eyre!(error.to_owned())),
+    }
 }
 
 pub fn calibrate(fpga: &mut Device) -> eyre::Result<()> {
@@ -37,8 +66,14 @@ pub fn calibrate(fpga: &mut Device) -> eyre::Result<()> {
         .into_iter()
         .map(|x| x as f64 / CALIBRATION_ACCUMULATIONS as f64)
         .collect();
+    // Fit to a polynomial
+    let channels: Vec<_> = (0..2048).map(|x| x as f64).collect();
+    let a_fit = polyfit(&channels, &a_norm, FIT_ORDER)?;
+    let b_fit = polyfit(&channels, &b_norm, FIT_ORDER)?;
     // FIXME write to file
     write_to_file(&a_norm, "a");
+    write_to_file(&a_fit, "a_fit");
     write_to_file(&b_norm, "b");
+    write_to_file(&b_fit, "b_fit");
     Ok(())
 }
